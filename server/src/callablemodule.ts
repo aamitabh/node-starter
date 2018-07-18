8aad89bf30ef6b1f493389c3a17a89138316b0b1
Arora, Amitabh <Amitabh.Arora@chevron.com>
2018-07-17 18:30:43 -0700

    Added types to the functions

--- server/src/callablemodule.ts
+++ server/src/callablemodule.ts
@@ -5,8 +5,21 @@ export function sayHello() {
   console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "Amitabh"}), undefined, 2)}`)
   console.log(`returnPerson = ${JSON.stringify(returnPerson({res: "Arora"}), undefined, 2)}`)
   console.log(`returnPerson = ${JSON.stringify(returnPerson({}), undefined, 2)}`)
+  console.log('--------------------------')
+  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "Amitabh", res: "Arora"}), undefined, 2)}`)
+  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "John"}), undefined, 2)}`)
+  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "Amitabh"}), undefined, 2)}`)
+  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({res: "Arora"}), undefined, 2)}`)
+  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({}), undefined, 2)}`)
+
 }
 
 export function returnPerson({req = "John", res = "Doe"}: {req?: string, res?: string}): { first: string; last: string } {
   return {first: req, last: res}
+}
+
+type httpreqres = {req?: string; res?: string}
+type user = {first?: string; last?: string}
+export function returnPersonBetter(rr: httpreqres): user {
+  return {first: rr.req, last: rr.res}
 }
