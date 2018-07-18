96c42afc70574d39a88c56b3b96e2f50a22a3eef
amitabh <buddhabacchan@gmail.com>
2018-07-18 07:08:00 -0700

    edited spaces in callmodules

--- server/src/callablemodule.ts
+++ server/src/callablemodule.ts
@@ -1,4 +1,7 @@
 export function sayHello() {
+  /*
+     Functions with object
+  */
   console.log(`Hello! time is ${Date.now()}`)
   console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "Amitabh", res: "Arora"}), undefined, 2)}`)
   console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "John"}), undefined, 2)}`)
@@ -6,20 +9,23 @@ export function sayHello() {
   console.log(`returnPerson = ${JSON.stringify(returnPerson({res: "Arora"}), undefined, 2)}`)
   console.log(`returnPerson = ${JSON.stringify(returnPerson({}), undefined, 2)}`)
   console.log('--------------------------')
-  console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "Amitabh", res: "Arora"}), undefined, 2)}`)
+
+  /*
+     Functions with typed object
+  */
+ console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "Amitabh", res: "Arora"}), undefined, 2)}`)
   console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "John"}), undefined, 2)}`)
   console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({req: "Amitabh"}), undefined, 2)}`)
   console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({res: "Arora"}), undefined, 2)}`)
   console.log(`returnPersonBetter = ${JSON.stringify(returnPerson({}), undefined, 2)}`)
-
 }
 
 export function returnPerson({req = "John", res = "Doe"}: {req?: string, res?: string}): { first: string; last: string } {
   return {first: req, last: res}
 }
 
-type httpreqres = {req?: string; res?: string}
+type httpReqRes = {req?: string; res?: string}
 type user = {first?: string; last?: string}
-export function returnPersonBetter(rr: httpreqres): user {
+export function returnPersonBetter(rr: httpReqRes): user {
   return {first: rr.req, last: rr.res}
 }
