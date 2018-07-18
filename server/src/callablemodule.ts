ee0b18561a0f1973700cd116a0bc7d3abe22344f
Arora, Amitabh <Amitabh.Arora@chevron.com>
2018-07-17 17:58:42 -0700

    Added more typescript examples

--- server/src/callablemodule.ts
+++ server/src/callablemodule.ts
@@ -1,3 +1,12 @@
 export function sayHello() {
   console.log(`Hello! time is ${Date.now()}`)
+  console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "Amitabh", res: "Arora"}), undefined, 2)}`)
+  console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "John"}), undefined, 2)}`)
+  console.log(`returnPerson = ${JSON.stringify(returnPerson({req: "Amitabh"}), undefined, 2)}`)
+  console.log(`returnPerson = ${JSON.stringify(returnPerson({res: "Arora"}), undefined, 2)}`)
+  console.log(`returnPerson = ${JSON.stringify(returnPerson({}), undefined, 2)}`)
+}
+
+export function returnPerson({req = "John", res = "Doe"}: {req?: string, res?: string}): { first: string; last: string } {
+  return {first: req, last: res}
 }
